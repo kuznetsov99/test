6d7642ceda01e0efb357d72727dfbb41f4fccb80
kuznetsov99 <skuznyetsov2@gmail.com>
2024-02-08 14:54:48 +0200

    Updated code structure before file handling

---
+++ main_lsd8_97124_0uc7.tsx
@@ -0,0 +1,4 @@
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+true / false
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
